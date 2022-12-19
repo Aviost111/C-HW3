139fcf7ff32c3946425aa0e24aeda05a73f2ec0f
Aviost111 <Aviostroff@yahoo.com>
2022-12-19 18:10:02 +0200

    work faster noa

--- functions.c
+++ functions.c
@@ -1,13 +1,15 @@
 #include "functions.h"
 #include <stdio.h>
 
-void shift_element(int*arr,int i){
-    int temp=*(arr+2);
-    for (int j = 1; j <i+1 ; ++j) {
-        *(arr+i)=temp;
-        if(j==i){
-            break;
-        }
-        temp=*(arr+i+1);
+void shift_element(int *arr, int i) {
+    int temp=*(arr+1) ,temp2;
+    for (int j = 1; j < i + 1; ++j) {
+        temp2=*(arr+j+1);
+        *(arr + j+1) = temp;
+        temp = temp2;
     }
+}
+
+void insertion_sort(int *arr, int len){
+
 }
