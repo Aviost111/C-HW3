afec4c70cdc881c6cfba105739a0adbd9e87e71e
Noa Amichai <Noa1254@gmail.com>
2022-12-19 18:14:24 +0200

    .

--- functions.c
+++ functions.c
@@ -1,15 +1,23 @@
 #include "functions.h"
 #include <stdio.h>
 
-void shift_element(int *arr, int i) {
-    int temp=*(arr+1) ,temp2;
-    for (int j = 1; j < i + 1; ++j) {
-        temp2=*(arr+j+1);
-        *(arr + j+1) = temp;
+void shift_element(int *arr, int i)
+{
+    int temp = *(arr + 1), temp2;
+    for (int j = 1; j < i + 1; ++j)
+    {
+        temp2 = *(arr + j + 1);
+        *(arr + j + 1) = temp;
         temp = temp2;
     }
 }
 
-void insertion_sort(int *arr, int len){
-
+void insertion_sort(int *arr, int len)
+{
+    int i, key;
+    for (i = 0; i < len; i++)
+    {
+        key = arr + i;
+        
+    }
 }
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #define SIZE 50
 
-//get 50 numbers from the user
+// get 50 numbers from the user
 void getNumbers(int *arr)
 {
     for (int i = 0; i < SIZE; i++)
