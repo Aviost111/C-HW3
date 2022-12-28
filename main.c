1bb2305f5c87cf8b09170933f3241f2aa619699d
Aviost111 <Aviostroff@yahoo.com>
2022-12-28 18:37:56 +0200

    finished before debug

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ void getNumbers(int *arr) {
 }
 
 int main() {
-    int arr[SIZE]={0};// declare array
-    getNumbers(arr);
+//    int arr[SIZE]={75, 43, 28, 29, 70, 76, 23, 96, 26, 2, 40, 21, 55, 13, 32, 56, 5, 64, 14, 77, 40, 70, 67, 38, 48, 59, 42, 42, 28, 6, 79, 46, 81, 26, 64, 66, 78, 58, 9, 1, 14, 44, 8, 31, 46, 25, 8, 32, 49, 93};// declare array
+    int arr[SIZE];
+//    getNumbers(arr);
     printf("stop");
     insertion_sort(arr, SIZE);
     for (int i = 0; i < SIZE-1; i++) // print sort array
