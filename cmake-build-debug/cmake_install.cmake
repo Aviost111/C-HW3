5a89ba3881ee75eaa4a814cd2e7945348bc77555
Aviost111 <Aviostroff@yahoo.com>
2022-12-28 15:11:31 +0200

    added all but last 2 methods

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/avi/CLionProjects/C-HW3
+# Install script for directory: /home/avi/Documents/GitHub/C-HW3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/avi/CLionProjects/C-HW3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/avi/Documents/GitHub/C-HW3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
