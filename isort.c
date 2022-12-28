16004773b5da18276f43236e1f1dcb0126928d22
Aviost111 <Aviostroff@yahoo.com>
2022-12-28 22:58:19 +0200

    finished sort

--- isort.c
+++ isort.c
@@ -11,13 +11,12 @@ void shift_element(int *arr, int i) {
 }
 
 void insertion_sort(int *arr, int len) {
-    int i, j, value,*temp;
+    int i, j, value;
     for (i = 1; i < len; i++) {
         value = *(arr + i);
         for (j = i - 1; j >= 0; j--) {
             if (value >= *(arr + j)) {
-                temp=(arr + j+1);
-                shift_element(temp, i - j-1);
+                shift_element((arr + j+1), i - j-1);
                 *(arr+j+1)=value;
                 break;
             }
