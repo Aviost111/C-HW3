be2e3c835d3d57cb701358e5e9ec58dab3aefc70
Aviost111 <Aviostroff@yahoo.com>
2022-12-28 22:56:34 +0200

    finished sort

--- isort.c
+++ isort.c
@@ -3,7 +3,7 @@
 
 void shift_element(int *arr, int i) {
     int temp = *(arr), temp2;
-    for (int j = 0; j < i + 1; ++j) {
+    for (int j = 0; j < i; ++j) {
         temp2 = *(arr + j + 1);
         *(arr + j + 1) = temp;
         temp = temp2;
@@ -11,18 +11,19 @@ void shift_element(int *arr, int i) {
 }
 
 void insertion_sort(int *arr, int len) {
-    int i, j, value;
-    printf("hi im here");
+    int i, j, value,*temp;
     for (i = 1; i < len; i++) {
         value = *(arr + i);
         for (j = i - 1; j >= 0; j--) {
             if (value >= *(arr + j)) {
-                shift_element((arr + j+1), i - j+1);
+                temp=(arr + j+1);
+                shift_element(temp, i - j-1);
+                *(arr+j+1)=value;
                 break;
             }
-            if((j==0)&&(value < *(arr + j))){
-                shift_element((arr + j), i);
-                break;
+            if((j==0)&&(value <= *(arr + j))){
+                shift_element(arr, i);
+                *(arr)=value;
             }
         }
     }
